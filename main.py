3edc9f50ec5b13a716b47689b753477a2627e241
Hknots <Hknots99@gmail.com>
2022-03-03 23:20:09 +0100

    All classes and methods are not finished. Also added a function to start the game.

--- main.py
+++ main.py
@@ -22,63 +22,64 @@ class Hangman:
 
     # Reminder to update
     def check_guess(self, guess):
-       # # Checking if the guess is valid by seeing if the user actually typed something
-       # if len(guess) < 1:
-       #     return print("You must guess at least one character")
-       
-       # # Checking if the guess is valid by only containing letters
-       # for letter in guess:
-       #     if letter not in self.alphabet:
-       #         return print("You can only guess letters")
-       #     else:
-       #         continue
-       # 
-       # if len(guess) > 1:
-       #     if guess == self.word:
-       #         for letter in guess:
-       #             if letter in self.word_letters:
-       #                 self.word_letters.remove(letter)
-       #             else:
-       #                 continue
-       #     else:
-       #         self.attempts += 1
-
-        if guess in self.word_letters:
-            self.guessed_letters.add(guess)
-            self.word_letters.remove(guess)
-        elif guess in self.guessed_letters:
-            print("Already guessed")
-        else:
-            self.guessed_letters.add(guess)
-            self.attempts += 1
+        # Checking if the guess is valid by seeing if the user actually typed something
+        if len(guess) < 1:
+            return print("\nYou must guess at least one character")
     
+        # Checking if the guess is valid by only containing letters
+        for letter in guess:
+            if letter not in self.alphabet:
+                return print("\nYou can only guess letters")
+        
+        # Checking if the guess is longer than one letter
+        if len(guess) > 1:
+            if guess == self.word:
+                for letter in guess: # For every letter in the guess, check if it in word_letters and remove it from the list, if it is.
+                    if letter in self.word_letters:
+                        self.word_letters.remove(letter)
+                    else:
+                        continue
+            else: # If the guess isnt the word, lose an attempt
+                self.attempts += 1
 
+        # Else if the guess is only one letter, check if its the correct
+        elif len(guess) == 1:
+            if guess in self.word_letters:
+                self.guessed_letters.add(guess)
+                self.word_letters.remove(guess)
+            elif guess in self.guessed_letters:
+                print("\nAlready guessed")
+            else:
+                self.guessed_letters.add(guess)
+                self.attempts += 1
+    
+    # Checks if the game is over
     def check_for_win(self):
         if len(self.word_letters) < 1:
             return True
     
-    
+    # Checks if the game is over
     def check_for_loss(self):
         if self.attempts >= self.max_attempts:
             return True
 
+# Function to start the game
 def game():
-    game = Hangman()
+    game = Hangman() # game assigned with class Hangman
     active = True
 
-    while active:
-        game.display()
-        guess = input("Guess: ").upper()
-        game.check_guess(guess)
+    while active: # Keeps playing, aslong as its active
+        game.display() # Displays Hangman, secret word, and guessed letters
+        guess = input("Guess: ").upper() # Lets user take a guess
+        game.check_guess(guess) # Checks if user guess is valid
 
-        if game.check_for_win():
-            print(f"You won the round! The secret word was {game.word}")
+        if game.check_for_win(): # Checks if player has won
+            print(f"\nYou won the round! The secret word was {game.word}")
             active = False
 
-        elif game.check_for_loss():
-            print()
-            print("H A N G M A N")
-            print(f"You lost. The secret word was {game.word}")
+        elif game.check_for_loss(): # Checks if player has lost
+            print("\nH A N G M A N")
+            print(f"\nYou lost. The secret word was {game.word}")
             active = False
 
-game()
+game() # Starts the game
